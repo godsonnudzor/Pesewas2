b59d23869564a834415c3e4c90c702da96e4cd8d
Godson Nudzor <godsonnudzor@gmail.com>
2026-06-01 12:12:17 +0000

    Starting the Backend

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
