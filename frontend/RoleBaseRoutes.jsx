f34a5f08f0fd95a9c3f4fbba7fe73cf466eb0065
Godson Nudzor <godsonnudzor@gmail.com>
2026-06-08 16:12:45 +0000

    Recoding on PrivateRoute and Rolebase

--- frontend/RoleBaseRoutes.jsx
+++ frontend/RoleBaseRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useAuth } from '../Context/authContext'
+import { useAuth } from '../context/authContext'
 import { Navigate } from 'react-router-dom'
 
 const RoleBaseRoutes = ({ children, reqiredRole, reqired, required, requiredRoles }) => {
